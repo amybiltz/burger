f4d553f6529de9d97923719d5f9f8e8e55ff39cb
amybiltz <amy.biltz@yahoo.com>
2019-03-16 23:40:48 -0700

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Select burgers_db
+USE burgers_db;
+
+-- insert 3 burgers
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
